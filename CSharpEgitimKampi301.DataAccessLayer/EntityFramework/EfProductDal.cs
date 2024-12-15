aba802ab28233c47e0e61ede29ec4bd253f46c54
DurdaneAkca <akcadurdane2002@gmail.com>
2024-12-15 16:26:16 +0300

    ...

--- CSharpEgitimKampi301.DataAccessLayer/EntityFramework/EfProductDal.cs
+++ CSharpEgitimKampi301.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Context;
 using CSharpEgitimKampi301.DataAccessLayer.Repositories;
 using CSharpEgitimKampi301.EntityLayer.Concrete;
 
@@ -11,5 +12,21 @@ namespace CSharpEgitimKampi301.DataAccessLayer.EntityFramework
 {
     public class EfProductDal : GenericRepository<Product>, IProductDal
     {
+        public List<Object> GetProductsWithCategory()
+        {
+            var context = new KampContext();
+            var values = context.Products
+                .Select(x => new
+                {
+                    ProductId = x.ProductId,
+                    ProductName = x.ProductName,
+                    ProductStock = x.ProductStock,
+                    ProductPrice = x.ProductPrice,
+                    ProductDescription = x.ProductDescription,
+                    CategoryName=x.Category.CategoryName,
+                }).ToList();
+
+            return values.Cast<Object>().ToList();
+        }
     }
 }
